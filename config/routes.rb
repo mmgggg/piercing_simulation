367741e00265afde61cf246306074c317263faa3
mmgggg <roc9.ggg@gmail.com>
2022-09-20 15:15:58 +0900

    toppageルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "top_page#top"
+
+  # root  'static_pages_controller#top'
+  
+  # resources :users, only: %i[new create]
+
+  # #ログイン機能のためのルーティング
+  # get 'login', to: 'user_sessions#new'
+  # post 'login', to: 'user_sessions#create'
+  # delete 'logout', to: 'user_sessions#destroy'
 end
