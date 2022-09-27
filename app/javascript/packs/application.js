57ce99859fd99efaf7eed1994cd551f6e276f4b0
mmgggg <roc9.ggg@gmail.com>
2022-09-27 14:05:26 +0900

    jsサンプルコード貼り付け"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../css/tailwindcss.css";
+//import "../css/tailwindcss.css";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+//自作jsファイル読み込み用
+import "./simulation/main"
