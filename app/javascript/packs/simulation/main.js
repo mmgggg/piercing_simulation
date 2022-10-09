78a4ec6300d815c1c746fb30acf8747d9273bfa5
mmgggg <roc9.ggg@gmail.com>
2022-10-09 17:47:31 +0900

    canvas上に耳画像を配置

--- app/javascript/packs/simulation/main.js
+++ app/javascript/packs/simulation/main.js
@@ -1,4 +1,17 @@
 document.addEventListener('turbolinks:load', () => {
+  //canvasに耳画像を描写する
+  window.onload = ()=>{
+    // canvas準備
+    const board = document.querySelector("#ear-image");
+    const ctx = board.getContext("2d");
+  
+    // 画像読み込み
+    const earImage = new Image();
+    earImage.src = "/ear_image_1920.jpg";  
+    earImage.onload = () => {
+      ctx.drawImage(earImage, 0, 0, 350, 450);
+    };
+  };
 
   //複製コード
   window.copy_image = function (clicked_id){
@@ -8,16 +21,24 @@ document.addEventListener('turbolinks:load', () => {
     var cloneElement = element.cloneNode(true);
     //複製したやつにlassを付け足す
     cloneElement.classList.add("drag-and-drop");
+    //コピーした画像にidを一つずつ付与
+    var head = "copy-image";
+    for(var i=0; i<=cloneElement.length-1; i++){
+      cloneElement.setAttribute("id", head+i);
+    }
+
     // class add-drag-and-dropに追加
     addImage = document.getElementsByClassName("add-drag-and-drop");
     addImage[0].appendChild(cloneElement);
-  };
-  
-  alert("Hi");
-  
+    //コピーした画像からonclick属性を削除
+    cloneElement.removeAttribute("onclick");
+    //コピーした画像からidを削除
+    //cloneElement.removeAttribute("id");
+  };  
+
   
   const move = new Moveable(document.body, {
-    target: document.querySelector(".pi4"),
+    target: document.querySelector("#image-4"),
     draggable: true,
     resizable: true,
     keepRatio: true,
@@ -39,20 +60,3 @@ document.addEventListener('turbolinks:load', () => {
   
   
 });
-
-
-// 成功複製コード
-//  複製コード
-//   var copyButton = document.getElementById("button-1");
-//   copyButton.addEventListener('click', function() {
-//     //コピーする対象をelementに代入
-//     var element = document.getElementById("image-1");
-//     // コピー元のノードを取得
-//     var cloneElement = element.cloneNode(true);
-//     //複製したやつにlassを付け足す
-//     cloneElement.classList.add("drag-and-drop");
-//     // class add-drag-and-dropに追加
-//     addImage = document.getElementsByClassName("add-drag-and-drop");
-//     addImage[0].appendChild(cloneElement);
-//   });
-  
