a220c583c5d17b8169fbda2d409eec73cc1a0ae7
mmgggg <roc9.ggg@gmail.com>
2022-10-11 16:49:16 +0900

    ログイン・ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+ 
   #ユーザー登録
   resources :users, only: %i[new create]
+
+  #ログイン機能
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 
   #管理者ページ
   namespace :admin do
