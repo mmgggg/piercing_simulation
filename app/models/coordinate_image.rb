72cbf72f96a63ff4ed899ccbe8e22b87df37110f
mmgggg <roc9.ggg@gmail.com>
2022-10-25 18:43:44 +0900

    bookmark機能追加

--- app/models/coordinate_image.rb
+++ app/models/coordinate_image.rb
@@ -1,7 +1,8 @@
 class CoordinateImage < ApplicationRecord
   mount_uploader :image, CoordinateImageUploader
   belongs_to :user
-
+  has_many :bookmarks, dependent: :destroy
+  
   validates :image, presence: true
   validates :user_id, presence: true
 end
