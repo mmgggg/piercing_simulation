1beb8641288f6711b9630fd77f4d513863b304f0
mmgggg <roc9.ggg@gmail.com>
2022-10-25 17:06:27 +0900

    hostを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,8 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'piercingsimulation' # 作成したバケット名を記述
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/piercingsimulation'
+    #config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/piercingsimulation'
+    config.asset_host = 'https://piercingsimulation.s3.ap-northeast-1.amazonaws.com'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
