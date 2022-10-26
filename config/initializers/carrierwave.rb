de91d0873d16d5376e2c0c88a21711bcb0df68bb
mmgggg <roc9.ggg@gmail.com>
2022-10-26 15:46:55 +0900

    host情報追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,7 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'piercingsimulation' 
     config.fog_public = false
+    config.asset_host = 'https://piercingsimulation.s3.ap-northeast-1.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
