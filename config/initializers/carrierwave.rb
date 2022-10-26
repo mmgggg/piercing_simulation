1ce7cb2fbcbb5512eaa2004c69c71b710b6a22cf
mmgggg <roc9.ggg@gmail.com>
2022-10-26 11:31:06 +0900

    Revert "環境変数・carrierwave.rb追記"
    This reverts commit 656db66dd716fe95cb2ec88a3af182561f2f8908.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,14 +6,12 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'piercingsimulation' # 作成したバケット名を記述
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/piercingsimulation'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーの環境変数
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキーの環境変数
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
       region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
       path_style: true
     }
 end
-
