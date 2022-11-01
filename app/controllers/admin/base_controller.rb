d9ba5f366e5e4050340eef93fecaa1ee5b2996a1
mmgggg <roc9.ggg@gmail.com>
2022-11-01 14:49:08 +0900

    管理者ログイン解除

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,19 +1,19 @@
 class Admin::BaseController < ApplicationController
-  before_action :check_admin
-  before_action :require_login
+  # before_action :check_admin
+  # before_action :require_login
   layout 'admin/layouts/application'
 
   private
 
-  def not_authenticated
-    flash[:warning] = "ログインしてください"
-    redirect_to admin_login_path
-  end
+  # def not_authenticated
+  #   flash[:warning] = "ログインしてください"
+  #   redirect_to admin_login_path
+  # end
 
-  def check_admin
-    unless current_user.admin?
-      flash[:warning] = "権限がありません"
-      redirect_to admin_login_path
-    end
-  end
+  # def check_admin
+  #   unless current_user.admin?
+  #     flash[:warning] = "権限がありません"
+  #     redirect_to admin_login_path
+  #   end
+  # end
 end
